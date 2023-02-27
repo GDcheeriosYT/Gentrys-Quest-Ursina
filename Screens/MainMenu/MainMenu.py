1faf00aabec7462168e401949de85793e499c2f9
GDcheerios <minecreeper0913@gmail.com>
2023-02-27 05:45:12 -0600

    worked on mainmenu screen

--- Screens/MainMenu/MainMenu.py
+++ Screens/MainMenu/MainMenu.py
@@ -9,6 +9,9 @@ from ursina import *
 
 class MainMenu:
     def __init__(self, version: str):
-        self.title = TitleText("Gentry's Quest")
+        self.title = TitleText("Gentry's Quest", hidden=True)
         self.version = VersionText(version)
         self.play_button = Button("Play", position=(0, -0.1), scale=(0.2, 0.05))
+        self.title.fade_in(1, 2)
+        self.audio = Audio("Audio/song.mp3", volume=0.1)
+        self.audio.fade_in(0.1, 2)
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from Graphics.Containers.Container import Container
 from Entity.Character.Character import Character
 from BraydenMesserschmidt.BraydenMesserschmidt import BraydenMesserschmidt
 
+# Screens
+from Screens.MainMenu.MainMenu import MainMenu
+
 # external packages
 from ursina import *
 
@@ -25,33 +28,7 @@ window.fps_counter.disable()
 
 # variables
 version = "Super Dooper Beta"
-VersionText(version)
 
-title = TitleText("Gentry's Quest", hidden=True)
-title.fade_in(1, 2)
-
-def poop():
-    BraydenMesserschmidt()
-
-play = Button("Play", position=(0, -0.1), scale=(0.2, 0.05))
-#play.on_click(poop)
-#play.on_click = print("big boogas")
-play.on_click = poop
-
-poop_thing = Container((0.5, 0))
-
-#poop_thing.
-
-poop_thing2 = Container((-0.5, 0), background_color=color.rgba(0, 0, 0, 255))
-
-poop_thing3 = Entity(model="quad", color=color.rgba(0, 0, 0, 255))
-poop_thing3 = Entity(model="quad", color=color.rgba(0, 0, 0, 50), position=(0, -0.3))
-
-poop_thing4 = Entity(model="quad", scale=(0.5, 0.3), position=(-0.5, 0), parent=camera.ui, color=color.black)
-poop_container = Entity(model="quad", scale=(0.25, 0.5), origin=(0.5, 0.5), position=(0.5, 0.5), parent=poop_thing4)
-
-
-#test_box = Entity(model="cube", scale=(0.2, 0.1, 0), position=(0.1, 0.1, 0), color=color.orange, parent=camera.ui)
-#print(test_box.position)
+main_menu = MainMenu(version)
 
 app.run()
