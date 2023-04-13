3330697e3db583e800a6aa66b556608c34cb2d18
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2023-04-13 07:08:02 -0500

    Removed print statements from EntityOverHead

--- Entity/EntityOverHead.py
+++ Entity/EntityOverHead.py
@@ -4,7 +4,6 @@ from ursina.prefabs.health_bar import HealthBar
 
 class EntityOverhead(Entity):
     def __init__(self, entity):
-        print(entity)
         super().__init__(
             model="quad",
             parent=entity,
@@ -13,7 +12,5 @@ class EntityOverhead(Entity):
             position=(0, 0.6),
             color=rgb(0, 0, 0, 0)
         )
-        print(entity.name)
-        print(entity.stats.health.get_value())
         self.name = Text(entity.name, parent=self, color=rgb(0, 0, 0, 255), scale=(5, 5), origin=(0, 0), position=(0, 0.2))
         self.health_bar = HealthBar(max_value=entity.stats.health.get_value(), parent=self, position=(-0.25, 0.1), show_text=False)
