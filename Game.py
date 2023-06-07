b9959b1019574760b321382db84fb9f3e6ec7091
GDcheerios <minecreeper0913@gmail.com>
2023-06-07 01:30:28 -0500

    added discord rich presence

--- Game.py
+++ Game.py
@@ -1,8 +1,11 @@
 import GameConfiguration
 from Overlays.NoficationsManager import NotificationManager
 from GameStates import GameStates
+from Online.Presence.Presence import GamePresence
+
 
 state = GameStates.intro
+presence = GamePresence()
 state_affected = False
 version = "Super Dooper Beta"
 user = None
@@ -14,5 +17,6 @@ notification_manager = NotificationManager
 def change_state(new_state: GameStates):
     global state
     global state_affected
+    presence.update_status(f"Currently in {new_state.name}")
     state = new_state
     state_affected = False
---
+++ Online/Presence/Presence.py
@@ -0,0 +1,16 @@
+from pypresence import Presence
+
+
+class GamePresence:
+    def __init__(self):
+        self.id = 1115885237910634587
+        self.RPC = Presence(self.id)
+        self.RPC.connect()
+
+    def update_status(self, state: str):
+        self.RPC.update(
+            state=state
+        )
+
+    def end(self):
+        self.RPC.close()
