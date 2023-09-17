e20f52e98cb314a46b117ad42c566e4a70983677
GDcheeriosYT <minecreeper0913@gmail.com>
2023-09-17 03:33:18 -0500

    started work on a ScrollableContainerTest

---
+++ Screens/Testing/TestingCategories/Components/Components.py
@@ -0,0 +1,11 @@
+from ...Category import Category
+
+
+class Components(Category):
+    def __init__(self):
+        super().__init__(
+            "Entity",
+            [
+                None
+            ]
+        )
