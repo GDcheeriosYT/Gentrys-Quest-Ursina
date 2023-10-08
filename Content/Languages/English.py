9181b7b8dc2b2c26a4820afa0ceef0b7c0387d87
GDcheeriosYT <minecreeper0913@gmail.com>
2023-10-08 21:43:04 +0800

    defaulted english language

--- Content/Languages/English.py
+++ Content/Languages/English.py
@@ -4,24 +4,5 @@ from Localization.Language import Language
 class English(Language):
     def __init__(self):
         super().__init__(
-            "English",
-            play="Play",
-            back="Back",
-            settings="Settings",
-            guest="Guest",
-            username="username",
-            create_guest="Create Guest",
-            login="Login",
-            login_not_available="Login not available yet...",
-            audio="Audio",
-            graphics="Graphics",
-            music="Music",
-            music_volume="Music Volume",
-            sound="Sound",
-            sound_volume="Sound Volume",
-            volume="Volume",
-            fullscreen="Fullscreen",
-            extra_ui_info="Extra UI Info",
-            apply="Apply",
-            applied_settings="Applied Settings"
+            "English"
         )
--- Localization/Language.py
+++ Localization/Language.py
@@ -3,25 +3,25 @@ class Language:
         self.name = name
 
         # langauge data
-        self.play = None
-        self.back = None
-        self.settings = None
-        self.guest = None
-        self.create_guest = None
-        self.username = None
-        self.login = None
-        self.login_not_available = None
-        self.audio = None
-        self.graphics = None
-        self.music = None
-        self.music_volume = None
-        self.sound = None
-        self.sound_volume = None
-        self.volume = None
-        self.fullscreen = None
-        self.extra_ui_info = None
-        self.apply = None
-        self.applied_settings = None
+        self.play = "Play"
+        self.back = "Back"
+        self.settings = "Settings"
+        self.guest = "Guest"
+        self.username = "username"
+        self.create_guest = "Create Guest"
+        self.login = "Login"
+        self.login_not_available = "Login not available yet..."
+        self.audio = "Audio"
+        self.graphics = "Graphics"
+        self.music = "Music"
+        self.music_volume = "Music Volume"
+        self.sound = "Sound"
+        self.sound_volume = "Sound Volume"
+        self.volume = "Volume"
+        self.fullscreen = "Fullscreen"
+        self.extra_ui_info = "Extra UI Info"
+        self.apply = "Apply"
+        self.applied_settings = "Applied Settings"
 
         for key, value in kwargs.items():
             setattr(self, key, value)
